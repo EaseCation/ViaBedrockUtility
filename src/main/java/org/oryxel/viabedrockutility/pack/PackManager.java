0c57671cd2f41d652b1c1121dd0731ae804ebb72
Oryxel <172038521+Oryxel@users.noreply.github.com>
2025-03-18 14:38:44 +0700

    Working pack downloader implementation.

--- src/main/java/org/oryxel/viabedrockutility/pack/PackManager.java
+++ src/main/java/org/oryxel/viabedrockutility/pack/PackManager.java
@@ -5,6 +5,10 @@ import org.oryxel.viabedrockutility.pack.content.Content;
 import org.oryxel.viabedrockutility.pack.definitions.EntityDefinitions;
 import org.oryxel.viabedrockutility.pack.definitions.ModelDefinitions;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 @Getter
 public class PackManager {
     private final Content content;
@@ -16,5 +20,12 @@ public class PackManager {
 
         this.entityDefinitions = new EntityDefinitions(this);
         this.modelDefinitions = new ModelDefinitions(this);
+
+        // Test
+        try {
+            Files.write(new File("output.zip").toPath(), content.toZip());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
